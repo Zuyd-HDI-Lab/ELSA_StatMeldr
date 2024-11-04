79c38a1d0d7ebfa9f7bcd2fd1af6da69656d5e7f
XergSolana <124872583+XergSolana@users.noreply.github.com>
2024-11-04 10:23:28 +0100

    Filter
    Give a dropdown menu of 2 cities, and show results based on city.

--- Statmeldr/Statmeldr/urls.py
+++ Statmeldr/Statmeldr/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+from api import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('cbs/', views.cbs_data, name='cbs_data'),
+    path('', RedirectView.as_view(url='/cbs/')),  
 ]
