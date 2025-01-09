5bc35783af02af6bbf1485cce13171937d63ca15
XergSolana <124872583+XergSolana@users.noreply.github.com>
2025-01-09 10:33:21 +0100

    button fixed
    download button works dynamically on both the cbs as the rivm side.

--- StatMeldr/Statmeldr/urls.py
+++ StatMeldr/Statmeldr/urls.py
@@ -25,5 +25,5 @@ urlpatterns = [
     path('', views.get_filtered_data, name='get_filtered_data'),
     path('rivm/', views.get_rivm_data, name='get_rivm_data'),
     path('cbs/', views.get_filtered_data, name='get_filtered_data'),
-    path('get-data-csv/', views.get_filtered_data_csv, name='get_filtered_data_csv'),
+    path('download_csv/', views.get_filtered_data_csv, name='get_filtered_data_csv'),
 ]
